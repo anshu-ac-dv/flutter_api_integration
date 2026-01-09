2d1636bb4bfed1d4f41c06174104884854c2b1a9
anshu <anshu.ac.dv@gmail.com>
2026-01-09 16:10:23 +0530

    Initial Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:api_integration/models/PostModel.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +11,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postList = [];
+  Future<List<PostModel>> getPostApi() async {
+    final resposne = await http.get(
+      Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+    );
+    var data = jsonDecode(resposne.body.toString());
+    if (resposne.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +37,25 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: postList.length,
+                    itemBuilder: (context, index) {
+                      return Text(postList[index].body.toString());
+                    },
+                  );
 
+                }
+                else {
+                  return Text("Loading....");
+                }
+              },
+            ),
+          ),
         ],
       ),
     );
